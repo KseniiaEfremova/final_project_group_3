cb599715a5ce03162080287b43f9e18bae6ba606
k.stopczynska <k.stopczynska@gmail.com>
2023-11-16 12:13:29 +0100

    added run function that diplays new board game

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+import pygame
+import sys
+
+class Board():
+	def __init__(self, name, res, frames):
+		self.name = name
+		self.res = res
+		self.frames = frames
+
+	def display_board(self):
+		pygame.init()
+		pygame.display.set_caption(self.name)
+		board = pygame.display.set_mode(self.res)
+		fps = pygame.time.Clock()
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					sys.exit()
+			pygame.display.update()
+			fps.tick(self.frames)
+
+
 def run():
-	print('Hello fellow coder!')
+	game_board = Board('arcade catcher', (800, 600), 60)
+	game_board.display_board()
+
 
 
 if __name__ == '__main__':
