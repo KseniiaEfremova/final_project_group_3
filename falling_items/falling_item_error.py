11de54ec60a645efbc914436d0a2d761463f3b3b
Sadaf Hamidi <sadaf@Sadafs-MacBook-Air.local>
2023-11-17 18:59:40 +0000

    Created file and added code for falling item error

---
+++ falling_items/falling_item_error.py
@@ -0,0 +1,19 @@
+from abstract_falling_item import FallingItem
+import random
+
+class Error(FallingItem):
+    def __init__(self, board_width):
+        super().__init__('Error', 8, 10, -5, 30, 30, 0, 0)
+        self.board_width = board_width
+        self.spawn()
+
+    def spawn(self):
+        self.x = random.randint(0, self.board_width - self.width)
+        self.y = 0
+
+    def fall(self):
+        self.y += self.speed
+
+    def disappear(self):
+        return self.y > 600
+    
