e5d333111dfc78937045026f8b7994aba0d5125e
Kerri Tanya <kerritg1@gmail.com>
2023-11-18 17:19:25 +0000

    made player image larger to 100 x 100, drew rectangle around player in the draw player function to check rectangle size as this is what is used for collisions

--- player.py
+++ player.py
@@ -1,18 +1,18 @@
 import pygame
 from board import Board
 
-
 # load player image
 player_image = pygame.image.load("assets/player.png")
 
 # Player class
 class Player(): 
     def __init__(self, x, y, board_instance: Board):
-        self.image = pygame.transform.scale(player_image, (45, 45))
+        self.image = pygame.transform.scale(player_image, (100, 100))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
         self.board_instance = board_instance
 
     def draw_player(self):
         self.board_instance.board.blit(self.image, self.rect)
+        pygame.draw.rect(self.board_instance.board, (255, 255, 255), self.rect, 2)
         
