5513fa311b784a2a09cc9a72f31a50aabdeae53e
Kerri Tanya <kerritg1@gmail.com>
2023-11-18 17:30:35 +0000

    edited the height ofthe rectangle to be 10 less, for more accurate collisions, and moved this in the y coordinate in the draw player

--- player.py
+++ player.py
@@ -8,11 +8,13 @@ player_image = pygame.image.load("assets/player.png")
 class Player(): 
     def __init__(self, x, y, board_instance: Board):
         self.image = pygame.transform.scale(player_image, (100, 100))
-        self.rect = self.image.get_rect()
+        self.width = 100
+        self.height = 90
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.rect.center = (x, y)
         self.board_instance = board_instance
 
     def draw_player(self):
-        self.board_instance.board.blit(self.image, self.rect)
+        self.board_instance.board.blit(self.image, (self.rect.x, self.rect.y - 10))
         pygame.draw.rect(self.board_instance.board, (255, 255, 255), self.rect, 2)
         
