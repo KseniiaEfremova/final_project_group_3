dcd1802ec93e79cc1d7241023e32c90e232b8f64
nataliapiotrowska <nataliapiotrowska@MacBook-Air-Natalia.local>
2023-11-18 18:22:48 +0100

    Setting up a file to work in

---
+++ falling_items/points_falling_item.py
@@ -0,0 +1,14 @@
+from abstract_falling_item import FallingItem
+
+# class for the items that award player with points
+
+
+class PointsFallingItem(FallingItem):
+    def spawn(self):
+        pass
+
+    def fall(self):
+        pass
+
+    def disappear(self):
+        pass
