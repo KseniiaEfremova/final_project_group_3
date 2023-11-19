6a1d4d462e395e805ab86e7b2d04e4ae10fb4e1b
Sadaf Hamidi <sadaf@Sadafs-MacBook-Air.local>
2023-11-19 02:51:36 +0000

    added while true loop

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from board import Board
 def run():
 	game_board = Board('arcade catcher', (800, 600), 60)
 	game_board.display_board()
+	error_item = ErrorItem(game_board.res[0])
+	
+	while True:
+		
+
 
 
 if __name__ == '__main__':
