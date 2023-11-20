7fb3a0308f7be4dc94008d53c6879931818ff831
nataliapiotrowska <nataliapiotrowska@MacBook-Air-Natalia.local>
2023-11-20 17:59:17 +0100

    changed to have three subclasses instead of three instances of one class

--- falling_items/points_falling_item.py
+++ falling_items/points_falling_item.py
@@ -1,3 +1,5 @@
+from abc import abstractmethod
+from abc import ABC
 from abstract_falling_item import FallingItem
 
 # class for the items that award player with points
@@ -9,20 +11,29 @@ class PointsFallingItem(FallingItem):
             name, speed, damage, points, width, height, x, y
         )
 
-    def spawn(self):
-        pass
-
-    def fall(self):
-        pass
-
     def disappear(self):
-        pass
+        return self.y > 600
 
 
-# creating three instances of the PointsFallingItem class
-PointsFallingItem1 = PointsFallingItem("tick", 5, 0, 1, 0, 0, 0, 0)
+class TickFallingItem(PointsFallingItem):
+    def __init__(self, name, speed, damage, points, width, height, x, y):
+        super().__init__(
+            "tick", 5, 0, 1, 0, 0, 0, 0
+        )
 
-PointsFallingItem2 = PointsFallingItem("python", 12, 0, 5, 0, 0, 0, 0)
 
-PointsFallingItem3 = PointsFallingItem("rubber duck", 8, 0, 10, 0, 0, 0, 0)
+class PythonFallingItem(PointsFallingItem):
+    def __init__(self, name, speed, damage, points, width, height, x, y):
+        super().__init__(
+            "python", 12, 0, 5, 0, 0, 0, 0
+        )
 
+
+class RubberDuckFallingItem(PointsFallingItem):
+    def __init__(self, name, speed, damage, points, width, height, x, y):
+        super().__init__(
+            "rubber duck", 8, 0, 10, 0, 0, 0, 0
+        )
+
+
+ducky = RubberDuckFallingItem()
