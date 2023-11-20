6ccc54f952b5ed10c18ec7f1996612023bd12aa6
Sadaf Hamidi <sadaf@Sadafs-MacBook-Air.local>
2023-11-20 17:22:42 +0000

    Added items to run

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ def run():
 	game_board = Board('arcade catcher', (800, 600), 60)
 	game_board.display_board()
 	error_item = ErrorItem(game_board.res[0])
+	warning_item = WarningItem(game_board.res[0])
+	bug_item = BugItem(game_board.res[0])
 	
-	#falling_items = [error_item]
+	falling_items = [error_item, warning_item,bug_item]
 
 	#While True: 
-	# for item in falling_items:
-	#     item.fall()
-	#     item.draw(game_board)
-	#     if item.disappear():
-	#        item.spawn()
+
+	#for item in falling_items:
+		#item.fall()
+		#item.draw(game_board)
+		#if item.disappear():
+			#item.spawn()
+
+	#game_board.update_display()
 
 
 if __name__ == '__main__':
