037234eb4e552ddbdad706a70adc7e4cf29673fd
Sadaf Hamidi <sadaf@Sadafs-MacBook-Air.local>
2023-11-20 12:53:57 +0000

    Commented my code to run falling_items

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ def run():
 
 	game_board = Board('arcade catcher', (800, 600), 60)
 	game_board.display_board()
-	error_item = ErrorItem(game_board.res[0])
+	#error_item = ErrorItem(game_board.res[0])
 	
-	#will add these changes later on
-	#while True:
-		#error_item.fall()
-		#error_item.draw(game_board) 
+	#falling_items = [error_item]
 
-		#if error_item.disappear():
-			#error_item.spawn()
+	#While True: 
+	# for item in falling_items:
+	#     item.fall()
+	#     item.draw(game_board)
+	#     if item.disappear():
+	#        item.spawn()
 
 
 if __name__ == '__main__':
