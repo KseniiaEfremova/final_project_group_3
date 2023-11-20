c60d095516bb6634820590c8431b4a082c388deb
nataliapiotrowska <nataliapiotrowska@MacBook-Air-Natalia.local>
2023-11-20 11:03:07 +0100

    Added three instances of the points falling item class

--- falling_items/points_falling_item.py
+++ falling_items/points_falling_item.py
@@ -4,6 +4,11 @@ from abstract_falling_item import FallingItem
 
 
 class PointsFallingItem(FallingItem):
+    def __init__(self, name, speed, damage, points, width, height, x, y):
+        super().__init__(
+            name, speed, damage, points, width, height, x, y
+        )
+
     def spawn(self):
         pass
 
@@ -12,3 +17,12 @@ class PointsFallingItem(FallingItem):
 
     def disappear(self):
         pass
+
+
+# creating three instances of the PointsFallingItem class
+PointsFallingItem1 = PointsFallingItem("tick", 5, 0, 1, 0, 0, 0, 0)
+
+PointsFallingItem2 = PointsFallingItem("python", 12, 0, 5, 0, 0, 0, 0)
+
+PointsFallingItem3 = PointsFallingItem("rubber duck", 8, 0, 10, 0, 0, 0, 0)
+
