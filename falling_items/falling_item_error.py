2c6c1507867f89f48486a558f543c6210246f750
Sadaf Hamidi <sadaf@Sadafs-MacBook-Air.local>
2023-11-20 12:55:45 +0000

    added board_width to super

--- falling_items/falling_item_error.py
+++ falling_items/falling_item_error.py
@@ -3,20 +3,8 @@ import random
 
 class ErrorItem(FallingItem):
     def __init__(self, board_width):
-        super().__init__('Error', 8, 10, -5, 30, 30, 0, 0)
-        self.board_width = board_width
-        self.spawn()
-
-    def spawn(self):
-        self.x = random.randint(0, self.board_width - self.width)
-        self.y = 0
-
-    def fall(self):
-        self.y += self.speed
+        super().__init__('Error', 8, 10, -5, 30, 30, 0, 0, board_width)
 
     def disappear(self):
         return self.y > 600
-    
-    def draw(self, board_instance):
-        pygame.draw.rect(board_instance.board, (255, 0, 0), (self.x, self.y, self.width, self.height))
     
