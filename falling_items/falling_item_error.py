7083dfb57612bf3b53b9bb449c8b9f78c3bfbb9f
Sadaf Hamidi <sadaf@Sadafs-MacBook-Air.local>
2023-11-20 17:18:03 +0000

    Created class DamageFallingItem) git push

--- falling_items/falling_item_error.py
+++ falling_items/falling_item_error.py
@@ -1,10 +1,28 @@
 from abstract_falling_item import FallingItem
 import random
 
-class ErrorItem(FallingItem):
+class DamageFallingItem(FallingItem):
+    pass
+
+class ErrorItem(DamageFallingItem):
     def __init__(self, board_width):
         super().__init__('Error', 8, 10, -5, 30, 30, 0, 0, board_width)
 
     def disappear(self):
         return self.y > 600
+    
+class BugItem(DamageFallingItem):
+    def __init__(self, board_width):
+        super().__init__('Bug', 3, 30, -10, 30, 30, 0, 0, board_width)
+    def disappear(self):
+        return self.y > 600
+    
+class WarningItem(DamageFallingItem):
+    def __init__(self, board_width):
+        super().__init__('Warning', 5, 1, -1, 30, 30, 0, 0, board_width)
+
+    def disappear(self):
+        return self.y > 600
+
+
     
