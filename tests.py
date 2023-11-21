6ca508581edb43548964091ebedd13ebbd516dc0
k.stopczynska <k.stopczynska@gmail.com>
2023-11-21 12:49:26 +0100

    added Player tests

--- tests.py
+++ tests.py
@@ -76,6 +76,7 @@ class TestPlayer(unittest.TestCase):
             (800, 600))
         self.test_board = Board("Test Board", (800, 600), 60)
 
+
     def test_player_initialization(self):
         player = Player(100, 100, self.test_board)
 
@@ -96,13 +97,29 @@ class TestPlayer(unittest.TestCase):
 
         self.assertLessEqual(total_diff, allowable_diff)
 
+    @patch('player.pygame.key.get_pressed')
+    def test_player_movement(self, mock_get_pressed):
+        self.player = Player(100, 100, self.test_board)
+        initial_x = self.player.rect.x
+        # check why this move is only to the right
+        self.player.move()
+        self.assertEqual(self.player.rect.x, initial_x + 10)
+
+    @patch('player.pygame.key.get_pressed')
+    def test_boundary_checking(self, mock_get_pressed):
+        self.player = Player(100, 100, self.test_board)
+        self.player.rect.x = 0
+        # check why this move is only to the right
+        self.player.move()
+        self.assertEqual(self.player.rect.x, 10)
+
+        self.player.rect.x = self.test_board.res[0] - self.player.width
+        self.player.move()
+        expected_x = self.test_board.res[0] - self.player.width
+        self.assertEqual(self.player.rect.x, expected_x)
+
     def tearDown(self):
         pygame.quit()
-
-
-
-
-
 
 
 
