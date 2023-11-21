af4da28b112a5e341e684cc7416649202e6115a1
KSENIIA EFREMOVA <ef.xenya@yandex.ru>
2023-11-21 14:00:08 +0000

    Add db_utils with functions to connect/create to DB

---
+++ db_utils.py
@@ -0,0 +1,69 @@
+import mysql.connector
+import logging
+from mysql.connector import errorcode
+from config import data
+
+host, user, password = data["host"], data["user"], data["passwd"]
+DB_NAME = 'game_users_db'
+
+
+def connect_to_mysql_database(db_name):
+    """
+    Connects to a MySQL database using credentials from a JSON file.
+    Returns:
+        mysql.connector.MySQLConnection: A MySQL database connection object.
+    Raises:
+        mysql.connector.Error: If there is an error during the database connection process.
+    """
+    try:
+        db_connection = mysql.connector.connect(
+            host=host,
+            user=user,
+            passwd=password,
+            auth_plugin='mysql_native_password',
+            database=db_name
+        )
+        return db_connection
+    except mysql.connector.Error as e:
+        print(f"Error connecting to MySQL database: {e}")
+        raise e
+    except ValueError as e:
+        print(f"ValueError: {e}")
+        raise e
+    except Exception as e:
+        # Catch unexpected exceptions, log them, and raise to crash the program
+        logging.exception(f"Unexpected error occurred: {e}")
+        raise e
+
+
+def get_cursor_and_connection(db_name):
+    db_connection = connect_to_mysql_database(db_name)
+    cursor = db_connection.cursor()
+    return cursor, db_connection
+
+
+def create_database(db_name):
+    try:
+        cursor, _ = get_cursor_and_connection(db_name)
+        cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(db_name))
+
+    except mysql.connector.Error as err:
+        print("Failed creating database: {}".format(err))
+        exit(1)
+
+
+def connect_to_database_or_create_if_not_exists(db_name):
+    try:
+        cursor, db_connection = get_cursor_and_connection(db_name)
+        cursor.execute("USE {}".format(db_name))
+    except mysql.connector.Error as err:
+        print("Database {} does not exists.".format(db_name))
+        if err.errno == errorcode.ER_BAD_DB_ERROR:
+            create_database(db_name)
+            print("Database {} created successfully.".format(db_name))
+            db_connection.database = db_name
+        else:
+            print(err)
+            exit(1)
+    print(f"You are using {db_name} database.")
