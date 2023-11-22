dada125544f4b3231781c3e2fb3876f3bf5dcef9
k.stopczynska <k.stopczynska@gmail.com>
2023-11-22 18:52:04 +0100

    added shrinking animation

--- falling_items/damage_falling_item.py
+++ falling_items/damage_falling_item.py
@@ -22,14 +22,23 @@ class DamageFallingItem(FallingItem, ABC):
         elapsed_time = current_time - stop_time
 
         if elapsed_time <= datetime.timedelta(seconds=3):
-            disappearance_progress = elapsed_time.total_seconds() / 3
-            scaled_width = max(int(self.width + 60 * disappearance_progress), 1)
-            scaled_height = max(int(self.height + 60 * disappearance_progress),
-                                1)
+            disappearance_progress = -(elapsed_time.total_seconds() / 3)
+
+            initial_width = self.width
+            initial_height = self.height
+            scaled_width = int(initial_width + 90 * disappearance_progress)
+            scaled_height = int(initial_height + 90 * disappearance_progress)
+
+            final_width = max(scaled_width, initial_width)
+            final_height = max(scaled_height, initial_height)
+
             blow = pygame.transform.scale(self.image,
-                                          (scaled_width, scaled_height))
-            self.board_instance.board.blit(blow, (
-            self.x - self.width, self.y - self.height))
+                                          (final_width, final_height))
+
+            offset_x = self.x - (final_width - initial_width) // 2
+            offset_y = self.y - (final_height - initial_height) // 2
+
+            self.board_instance.board.blit(blow, (offset_x, offset_y))
         if datetime.datetime.utcnow() > stop_time:
             self.image.fill(transparent)
             self.kill()
