9712fc8c5eeca38ab3ec73ce3531c42088c87bb5
k.stopczynska <k.stopczynska@gmail.com>
2023-11-23 19:24:16 +0100

    added warning item tests

--- test_error_item.py
+++ test_error_item.py
@@ -17,6 +17,7 @@ class TestErrorItem(unittest.TestCase):
 
     def test_error_item_init(self):
         error_item = ErrorItem(error_image, self.test_board)
+
         self.assertEqual(error_item.width, 30)
         self.assertEqual(error_item.height, 30)
         self.assertEqual(error_item.y, 0)
@@ -28,7 +29,6 @@ class TestErrorItem(unittest.TestCase):
         error_item = ErrorItem(error_image, self.test_board)
         error_item.draw(self.test_board)
         error = pygame.surfarray.array3d(error_item.image)
-        # loaded_error = pygame.image.load("assets/error.png")
         loaded_error = pygame.transform.scale(error_image, (30, 30))
         error_content = pygame.surfarray.array3d(loaded_error)
         diff = np.abs(error_content - error)
@@ -39,11 +39,11 @@ class TestErrorItem(unittest.TestCase):
 
     def test_error_item_spawn(self):
         error_item = ErrorItem(error_image, self.test_board)
+
         self.assertFalse(error_item.x < 0)
         self.assertFalse(error_item.x == 0)
         self.assertFalse(error_item.x > 770)
         self.assertTrue(0 < error_item.x < 770)
-
 
     def test_error_item_fall(self):
         error_item = ErrorItem(error_image, self.test_board)
