b053a09ac5a22e6fc4795dd01a02588d58fefd37
k.stopczynska <k.stopczynska@gmail.com>
2023-11-24 14:36:16 +0100

    animating player to right

--- player.py
+++ player.py
@@ -4,9 +4,21 @@ from board import Board
 player_image = pygame.image.load("assets/player_idle.png")
 
 
-class Player:
+class Player(pygame.sprite.Sprite):
     def __init__(self, x, y, board_instance: Board):
-        self.image = pygame.transform.scale(player_image, (100, 238))
+        super().__init__()
+        self.sprites = []
+        self.sprites.append(pygame.image.load("assets/player_idle.png"))
+        self.sprites.append(pygame.image.load("assets/player1.png"))
+        self.sprites.append(pygame.image.load("assets/player2.png"))
+        self.sprites.append(pygame.image.load("assets/player3.png"))
+        self.sprites.append(pygame.image.load("assets/player4.png"))
+        self.sprites.append(pygame.image.load("assets/player5.png"))
+        self.sprites.append(pygame.image.load("assets/player6.png"))
+        self.sprites.append(pygame.image.load("assets/player7.png"))
+        self.sprites.append(pygame.image.load("assets/player8.png"))
+        self.current_sprite = 0
+        self.image = pygame.transform.scale(self.sprites[self.current_sprite], (100, 238))
         self.width = 100
         self.height = 90
         self.rect = pygame.Rect(0, 0, self.width, self.height)
@@ -24,6 +36,14 @@ class Player:
             dx = self.board_instance.res[0] - self.rect.right
         return dx, dy
 
+    def animate(self):
+        self.current_sprite += 1
+        if self.current_sprite >= 8:
+            self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
+        self.image = pygame.transform.scale(self.sprites[self.current_sprite],
+                                            (100, 238))
+
     def move(self):
         # variable to reset for delta x and delta y
         dx = 0
@@ -31,9 +51,10 @@ class Player:
         key = pygame.key.get_pressed()
         if key[pygame.K_a] or key[pygame.K_LEFT]:
             dx = -10
+            self.animate()
         if key[pygame.K_d] or key[pygame.K_RIGHT]:
             dx = 10
-
+            self.animate()
         dx, dy = self.constrain_move_within_board(dx, dy)    
 
         self.rect.x += dx
