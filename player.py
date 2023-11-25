ca481c921ea8d9e8e508dba94504cc26756cc0c7
Kerri Tanya <kerritg1@gmail.com>
2023-11-25 17:41:06 +0000

    added logic to check_falling_item_collision() method in player.py, which moved the rectangle of a falling item when hit out into space, so that points/damage don't keep accumulating

--- player.py
+++ player.py
@@ -3,12 +3,11 @@ from board import Board
 from falling_items.points_falling_item import PythonItem, TickItem, RubberDuckItem
 from falling_items.damage_falling_item import WarningItem, ErrorItem, BugItem
 from typing import List
-from falling_items.abstract_falling_item import FallingItem
 import datetime
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y, board_instance: Board, python_instance: PythonItem, tick_instance: TickItem, duck_instance: RubberDuckItem, warning_instance: WarningItem, error_instance: ErrorItem, bug_instance: BugItem):
+    def __init__(self, x, y, board_instance: Board, python_instance: PythonItem, tick_instance: TickItem, duck_instance: RubberDuckItem, warning_instance: WarningItem, error_instance: ErrorItem, bug_instance: BugItem):        
         super().__init__()
         self.sprites_right = []
         self.sprites_right.append(pygame.image.load("assets/player_idle.png"))
@@ -49,6 +48,7 @@ class Player(pygame.sprite.Sprite):
         self.error_instance = error_instance
         self.bug_instance = bug_instance
         
+        
     def draw_player(self):
         self.board_instance.board.blit(self.image, (self.rect.x,
                                                     self.rect.y - 10))
@@ -59,6 +59,7 @@ class Player(pygame.sprite.Sprite):
         if self.rect.right + dx > self.board_instance.res[0]:
             dx = self.board_instance.res[0] - self.rect.right
         return dx, dy
+    
 
     def animate(self, direction):
         self.current_sprite += 1
@@ -92,66 +93,44 @@ class Player(pygame.sprite.Sprite):
         
     
     def check_falling_item_collision(self):
-        print(self.python_instance.rect)
         if self.rect.colliderect(self.python_instance.rect):
-            self.height = 300
             print("You have hit the Python")
             self.points += self.python_instance.points
             self.damage += self.python_instance.damage
-            self.python_instance.disappear(datetime.datetime.utcnow() + datetime.timedelta(seconds=3))
+            self.python_instance.rect.topleft = (-100, -100)
             print(f"Player Points: {self.points}, Player Damage: {self.damage}")
 
         elif self.rect.colliderect(self.tick_instance.rect):
-            self.height = 300
             print("You have hit the tick")
             self.points += self.tick_instance.points
             self.damage += self.tick_instance.damage
-            self.tick_instance.disappear(datetime.datetime.utcnow() + datetime.timedelta(seconds=3))
+            self.tick_instance.rect.topleft = (-100, -100)
             print(f"Player Points: {self.points}, Player Damage: {self.damage}")
-            
+        
         elif self.rect.colliderect(self.duck_instance.rect):
-            self.height = 300
             print("You have hit the Duck")
             self.points += self.duck_instance.points
             self.damage += self.duck_instance.damage
-            self.duck_instance.disappear(datetime.datetime.utcnow() + datetime.timedelta(seconds=3))
+            self.duck_instance.rect.topleft = (-100, -100)
             print(f"Player Points: {self.points}, Player Damage: {self.damage}")
             
         elif self.rect.colliderect(self.warning_instance.rect):
-            self.height = 300
             print("You have hit the Warning")
             self.points += self.warning_instance.points
             self.damage += self.warning_instance.damage
-            self.warning_instance.disappear(datetime.datetime.utcnow() + datetime.timedelta(seconds=3))
+            self.warning_instance.rect.topleft = (-100, -100)
             print(f"Player Points: {self.points}, Player Damage: {self.damage}")
             
         elif self.rect.colliderect(self.error_instance.rect):
-            self.height = 300
             print("You have hit the Error")
             self.points += self.error_instance.points
             self.damage += self.error_instance.damage
-            self.error_instance.disappear(datetime.datetime.utcnow() + datetime.timedelta(seconds=3))
+            self.error_instance.rect.topleft = (-100, -100)
             print(f"Player Points: {self.points}, Player Damage: {self.damage}")
             
         elif self.rect.colliderect(self.bug_instance.rect):
-            self.height = 300
             print("You have hit the Bug")
             self.points += self.bug_instance.points
             self.damage += self.bug_instance.damage
-            self.bug_instance.disappear(datetime.datetime.utcnow() + datetime.timedelta(seconds=3))
+            self.bug_instance.rect.topleft = (-100, -100)
             print(f"Player Points: {self.points}, Player Damage: {self.damage}")
-    
-    ## If I get the above working I will try to put it in a list to simplify
-    # def check_falling_item_collision(self):
-    #     for falling_item in self.falling_items:
-    #         if self.rect.colliderect(falling_item.rect):
-    #             self.height = 300
-    #             print(f"You have hit the {falling_item.name}")
-
-    #             # Update player's points and damage 
-    #             self.points += falling_item.points
-    #             self.damage += falling_item.damage
-
-    #             falling_item.disappear(datetime.datetime.utcnow() + datetime.timedelta(seconds=3))
-
-    #             print(f"Player Points: {self.points}, Player Damage: {self.damage}")
