0e7a20d8963593731b15d8e1c2829c49a8e14aa7
k.stopczynska <k.stopczynska@gmail.com>
2023-11-27 17:30:07 +0100

    added creating and destroying projectiles, and drawing a surface

--- menu/winning_menu.py
+++ menu/winning_menu.py
@@ -17,6 +17,7 @@ class WinningMenu(Menu):
 		self.new_fireworks = True
 		self.counter = 0
 		self.directions = [(1, 1), (1, 0), (0, 1), (-1, -1), (-1, 1), (1, -1), (-1, 0), (0, -1)]
+		self.fireworks_to_remove = []
 
 	def draw(self):
 		if self.new_fireworks:
@@ -25,19 +26,44 @@ class WinningMenu(Menu):
 				self.fireworks.append(firework)
 				self.new_fireworks = False
 
+			for i in range(len(self.fireworks) - 1):
+				if (self.fireworks[i]['delay'] < self.counter and
+					self.fireworks[i]['burst_y_pos'] < self.fireworks[i]['y_pos']):
+					pygame.draw.rect(self.board_instance.board_surface, self.fireworks[i]['color'],[self.fireworks[i]['x_pos'], self.fireworks[i]['y_pos'], 10, 10],0, 3 )
+					self.fireworks[i]['y_pos'] -= self.fireworks[i]['y_speed']
+				elif self.fireworks[i]['burst_y_pos'] >= self.fireworks[i]['y_pos']:
+					x_start = self.fireworks[i]['x_pos']
+					y_start = self.fireworks[i]['y_pos']
+					for j in range(len(self.directions)):
+						projectile = Projectile(x_start, y_start, self.directions[j][0] * 5, self.directions[j][1] * 5, self.fireworks[i]['color'])
+						self.projectiles.append(projectile)
+					self.fireworks_to_remove.append(i)
+			self.fireworks_to_remove.sort(reverse=True)
+			for r in self.fireworks_to_remove:
+				self.fireworks.remove(self.fireworks[r])
+			self.fireworks_to_remove = []
+			for i in range(len(self.projectiles)):
+				color = self.projectiles[i]['color'][0], self.projectiles[i]['color'][1], self.projectiles[i]['color'][2], self.projectiles[i]['delay'] * 4
+				pygame.draw.circle(self.board_instance.board_surface, color, (
+					self.projectiles[i]['x_start'], self.projectiles[i]['y_start']), 3)
+				self.projectiles[i]['delay'] -= 1
+				self.projectiles[i]['x_start'] += self.projectiles[i]['direction_x']
+				self.projectiles[i]['y_start'] += self.projectiles[i]['direction_y']
+				self.projectiles[i]['direction_y'] += 0.1
+				if self.projectiles[i]['delay'] < 0 or self.board_instance.res[0] < self.projectiles[i][
+					'x_start'] < -3 or self.board_instance.res[1] < self.projectiles[i]['y_start']:
+					self.fireworks_to_remove.append(i)
+				for p in range(len(self.fireworks_to_remove)):
+					self.projectiles.pop(0)
+				rect = pygame.Rect(0, 0, self.width, self.height)
+				win = pygame.Surface((self.width, self.height),
+									   pygame.SRCALPHA)
+				pygame.draw.rect(win, (255, 255, 255, 255), rect)
+				text = font.render("Congratulations! you won", True, (255, 255, 255))
+				self.board_instance.board.blit(win, (0, 0))
+				self.board_instance.board.blit(text, (150, 225))
 
-				for i in range(len(self.fireworks) - 1):
-					if (self.fireworks[i]['delay'] < self.counter and
-							self.fireworks[i]['burst_y_pos'] < self.fireworks[i]['y_pos']):
-						pygame.draw.rect(self.board_instance.board_surface, self.fireworks[i]['color'], [self.fireworks[i]['x_pos'], self.fireworks[i]['y_pos'],10, 10], 0, 3 )
-						self.fireworks[i]['y_pos'] -= self.fireworks[i]['y_speed']
-					elif self.fireworks[i]['burst_y_pos'] >= self.fireworks[i]['y_pos']:
-						x_start = self.fireworks[i]['x_pos']
-						y_start = self.fireworks[i]['y_pos']
-						for j in range(len(self.directions)):
-							projectile = Projectile(x_start, y_start, self.directions[j][0] * 5, self.directions[j][1] * 5, self.fireworks[i]['color'])
-							self.projectiles.append(projectile)
+			if len(self.fireworks) == 0 and len(self.projectiles) == 0:
+				self.counter = 0
+				self.new_fireworks = True
 
-				if len(self.fireworks) == 0 and len(self.projectiles) == 0:
-					self.counter = 0
-					self.new_fireworks = True
