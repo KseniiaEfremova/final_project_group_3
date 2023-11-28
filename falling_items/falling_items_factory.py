ecf9f7170031224ab01dcc1e0785aab153f19cef
nataliapiotrowska <nataliapiotrowska@MacBook-Air-Natalia.local>
2023-11-28 12:02:22 +0100

    oop falling items method - continuing work

--- falling_items/falling_items_factory.py
+++ falling_items/falling_items_factory.py
@@ -1,13 +1,17 @@
-from points_falling_item import TickItem, PythonItem, RubberDuckItem
-from damage_falling_item import ErrorItem, BugItem, WarningItem
+from falling_items.damage_falling_item import *
+from falling_items.points_falling_item import *
+from board import *
+from player import Player
 import pygame
 from abc import ABC
 import datetime
 import time
 import random
 
-class FallingItemsFactory(ABC):
+
+class FallingItemsFactory(ABC, pygame.sprite.Sprite):
     def __init__(self):
+        super().__init__()
         self.timer_seconds = 60
         self.start_time = time.time()
         self.long_stop = datetime.datetime.utcnow() + datetime.timedelta(
@@ -22,42 +26,82 @@ class FallingItemsFactory(ABC):
         self.counter = 0
         self.fall_timer = pygame.USEREVENT + 1
 
-        self.python_image = None
-        self.tick_image = None
-        self.duck_image = None
-        self.bug_image = None
-        self.error_image = None
-        self.warning_image = None
+        self.long_stop = datetime.datetime.utcnow() + datetime.timedelta(
+            seconds=5)
+        self.medium_stop = datetime.datetime.utcnow() + datetime.timedelta(
+            seconds=4)
+        self.short_stop = datetime.datetime.utcnow() + datetime.timedelta(
+            seconds=3)
 
-        self.load_assets()
-
-    def load_assets(self):
         self.python_image = pygame.image.load("assets/sprites/python1.png")
         self.tick_image = pygame.image.load("assets/sprites/tick.png")
         self.duck_image = pygame.image.load("assets/sprites/duck5.png")
         self.bug_image = pygame.image.load("assets/sprites/bug1.png")
         self.error_image = pygame.image.load("assets/sprites/error.png")
         self.warning_image = pygame.image.load("assets/sprites/warning.png")
+        self.falling_items = pygame.sprite.Group()
+        self.game_board = Board('Code Quest', (800, 600), 60)
+        self.python = PythonItem(self.python_image, self.game_board)
+        self.tick = TickItem(self.tick_image, self.game_board)
+        self.duck = RubberDuckItem(self.duck_image, self.game_board)
+        self.warning = WarningItem(self.warning_image, self.game_board)
+        self.error = ErrorItem(self.error_image, self.game_board)
+        self.bug = BugItem(self.bug_image, self.game_board)
+        self.item_list = []
 
     def create_timer(self):
         pygame.time.set_timer(self.fall_timer, self.interval)
 
-
     def create_group(self):
-        self.falling_items = pygame.sprite.Group()
-        self.item_list = [self.tick, self., self.warning, self.error, self.bug, self.python]
+        self.item_list = [self.tick, self.duck, self.warning, self.error, self.bug, self.python]
         for item in self.item_list:
             self.falling_items.add(item)
 
-    def events(self):
+    def set_fall_timer(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
             elif event.type == self.fall_timer:
                 self.counter += 1
                 print(f"Hi from timer: {self.counter}")
-                new_sprite = random.choice(item_list)
-                falling_items.add(new_sprite)
+                new_sprite = random.choice(self.item_list)
+                self.falling_items.add(new_sprite)
 
-                print(f"Falling items added to {falling_items}: {new_sprite}")
+                print(f"Falling items added to {self.falling_items}: {new_sprite}")
+
+    def fall_and_respawn(self):
+        for sprite in self.falling_items.sprites():
+            sprite.draw(self.game_board)
+            sprite.fall()
+            # if sprite.y >= 500:
+            #     sprite.disappear(self.short_stop)
+            if self.python.y >= 500:
+                self.python.disappear(self.medium_stop)
+            if self.tick.y >= 500:
+                self.tick.disappear(self.short_stop)
+            if self.duck.y >= 500:
+                self.duck.disappear(self.long_stop)
+            if self.warning.y >= 500:
+                self.warning.disappear(self.short_stop)
+            if self.error.y >= 500:
+                self.error.disappear(self.medium_stop)
+            if self.bug.y >= 500:
+                self.bug.disappear(self.long_stop)
+
+        if len(self.falling_items) >= 6:
+            for item in self.falling_items:
+                item.kill()
+        if self.python.y >= 500:
+            self.python.disappear(self.medium_stop)
+        if self.tick.y >= 500:
+            self.tick.disappear(self.short_stop)
+        if self.duck.y >= 500:
+            self.duck.disappear(self.long_stop)
+        if self.warning.y >= 500:
+            self.warning.disappear(self.short_stop)
+        if self.error.y >= 500:
+            self.error.disappear(self.medium_stop)
+        if self.bug.y >= 500:
+            self.bug.disappear(self.long_stop)
+
 
--- main.py
+++ main.py
@@ -1,56 +1,31 @@
 import random
-
+from falling_items.falling_items_factory import *
 from board import Board
 from player import Player
 from stats.life import Life
 from stats.level import Level
 from stats.timer import Timer
 from stats.points import Points
-from falling_items.points_falling_item import PythonItem, TickItem, RubberDuckItem
-from falling_items.damage_falling_item import WarningItem, ErrorItem, BugItem
 from decorators.sounds import Sounds
 import pygame
-import datetime
 import time
-
-# python_image = pygame.image.load("assets/sprites/python1.png")
-# tick_image = pygame.image.load("assets/sprites/tick.png")
-# duck_image = pygame.image.load("assets/sprites/duck5.png")
-# bug_image = pygame.image.load("assets/sprites/bug1.png")
-# error_image = pygame.image.load("assets/sprites/error.png")
-# warning_image = pygame.image.load("assets/sprites/warning.png")
+falling = FallingItemsFactory()
 
 @Sounds("assets/sounds/soundtrack.mp3", loop=True)
 def run():
     pygame.init()
     game_board = Board('Code Quest', (800, 600), 60)
-    python = PythonItem(python_image, game_board)
-    tick = TickItem(tick_image, game_board)
-    duck = RubberDuckItem(duck_image, game_board)
-    warning = WarningItem(warning_image, game_board)
-    error = ErrorItem(error_image, game_board)
-    bug = BugItem(bug_image, game_board)
-    player = Player(800 - 725, 600 - 200, game_board, python, tick, duck, warning, error, bug)
+    player = Player(800 - 725, 600 - 200, game_board, falling.python, falling.tick, falling.duck, falling.warning, falling.error, falling.bug)
     life = Life(player, game_board)
     level = Level(player, game_board)
     timer = Timer(player, game_board)
     points = Points(player, game_board)
-    # long_stop = datetime.datetime.utcnow() + datetime.timedelta(
-    #     seconds=5)
-    # medium_stop = datetime.datetime.utcnow() + datetime.timedelta(
-    #     seconds=4)
-    # short_stop = datetime.datetime.utcnow() + datetime.timedelta(
-    #     seconds=3)
-    # stop_list = [long_stop, medium_stop, short_stop]
 
-    # timer_seconds = 60
-    # start_time = time.time()
+    timer_seconds = 60
+    start_time = time.time()
 
     # timer for making items fall
-    fall_timer = pygame.USEREVENT + 1
-    interval = 1000
-    pygame.time.set_timer(fall_timer, interval)
-    counter = 0
+    falling.create_timer()
 
     while True:
         game_board.display_board()
@@ -58,77 +33,18 @@ def run():
         life.draw(game_board)
         level.draw(game_board)
         points.draw(game_board)
-        # python.draw(game_board)
-        # tick.draw(game_board)
-        # duck.draw(game_board)
-        # warning.draw(game_board)
-        # error.draw(game_board)
-        # bug.draw(game_board)
-        # python.fall()
-        # tick.fall()
-        # duck.fall()
-        # warning.fall()
-        # error.fall()
-        # bug.fall()
-        # if python.y >= 500:
-        #     python.disappear(medium_stop)
-        # if tick.y >= 500:
-        #     tick.disappear(short_stop)
-        # if duck.y >= 500:
-        #     duck.disappear(long_stop)
-        # if warning.y >= 500:
-        #     warning.disappear(short_stop)
-        # if error.y >= 500:
-        #     error.disappear(medium_stop)
-        # if bug.y >= 500:
-        #     bug.disappear(long_stop)
 
-        # current_time = time.time()
-        # elapsed_time = current_time - start_time
-        # remaining_time = max(timer_seconds - int(elapsed_time), 0)
-        # timer.draw(game_board, timer=remaining_time)
+        current_time = time.time()
+        elapsed_time = current_time - start_time
+        remaining_time = max(timer_seconds - int(elapsed_time), 0)
+        timer.draw(game_board, timer=remaining_time)
         
         player.draw_player()
         player.move()
 
-        # falling_items = pygame.sprite.Group()
-        # item_list = [tick, duck, warning, error, bug, python]
-        # for item in item_list:
-        #     falling_items.add(item)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-            elif event.type == fall_timer:
-                counter += 1
-                print(f"Hi from timer: {counter}")
-                new_sprite = random.choice(item_list)
-                falling_items.add(new_sprite)
-
-                print(f"Falling items added to {falling_items}: {new_sprite}")
-
-        for sprite in falling_items.sprites():
-            sprite.draw(game_board)
-            sprite.fall()
-            if sprite.y >= 500:
-                sprite.disappear(short_stop)
-
-        if len(falling_items) >= 6:
-            for item in falling_items:
-                item.kill()
-
-        # if python.y >= 500:
-        #     python.disappear(medium_stop)
-        # if tick.y >= 500:
-        #     tick.disappear(short_stop)
-        # if duck.y >= 500:
-        #     duck.disappear(long_stop)
-        # if warning.y >= 500:
-        #     warning.disappear(short_stop)
-        # if error.y >= 500:
-        #     error.disappear(medium_stop)
-        # if bug.y >= 500:
-        #     bug.disappear(long_stop)
+        falling.create_group()
+        falling.set_fall_timer()
+        falling.fall_and_respawn()
 
         player.check_falling_item_collision()
         game_board.update_display()
