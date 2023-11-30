f16e71d9072e5259e93c9565b9f402f95cef09de
KSENIIA EFREMOVA <ef.xenya@yandex.ru>
2023-11-30 17:44:22 +0000

    Add functions to check valid password and username, hash_password,  check_username_and_password, is_user_exist_in_db, add_valid_user_data_to_db

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 #  manipulating with user's data in DB
 from db_utils import get_cursor_and_connection
+import re
+import hashlib
+
+DB_NAME = "game_users_db"
+users_table = "users"
 
 
 # adding new user
@@ -86,5 +91,76 @@ def get_user_id(db_name, table_name, username):
         if db_connection:
             db_connection.close()
             print("DB connection is closed")
-
+    if not user_id:
+        return
     return user_id[0][0]
+
+
+def is_valid_username(username: str) -> bool:
+    # Check if the username meets the criteria
+    # Between 3 and 20 characters
+    # Contains only letters, numbers, and underscores
+    return re.match("^[a-zA-Z0-9_]{3,20}$", username) is not None
+
+
+def is_valid_password(password: str) -> bool:
+    # Check if the password meets the criteria
+    # At least 8 characters long
+    # Contains at least one uppercase letter
+    # Contains at least one lowercase letter
+    # Contains at least one digit
+    # Contains at least one special character (e.g., !@#$%^&*)
+    return (
+            len(password) >= 8 and
+            any(c.isupper() for c in password) and
+            any(c.islower() for c in password) and
+            any(c.isdigit() for c in password) and
+            re.search("[!@#$%^&*]", password) is not None
+    )
+
+
+def hash_password(password: str) -> str:
+    """
+    Hashes a password using SHA-256 algorithm with a salt.
+    """
+    # Use a static salt for hashing
+    salt = "weqcrh378451#&*$3i4ycn24utyvn6y34y!(@*74"
+
+    # Hash the password using SHA-256
+    hashed_password = hashlib.sha256((password + ":" + salt).encode('utf-8')).hexdigest()
+    return hashed_password
+
+
+def check_username_and_password(input_username: str, input_password: str) -> tuple or None:
+    """
+    Checks if the provided username and password are valid.
+
+    Parameters:
+    - input_username (str): The input username.
+    - input_password (str): The input password.
+
+    Returns:
+    - Tuple[str, str] or None: A tuple containing the valid username and password,
+      or None if either the username or password is invalid.
+    """
+    if not is_valid_username(input_username) or not is_valid_password(input_password):
+        return None
+    return input_username, input_password
+
+
+def is_user_exist_in_db(db_name: str, table_name: str, username: str) -> bool:
+    """Checks if a user with the given username exists in the specified database table."""
+    if get_user_id(db_name, table_name, username) is None:
+        return False
+    return True
+
+
+def add_valid_user_data_to_db(username: str, password: str):
+    """Adds valid user data to the specified database if the user does not already exist."""
+    if is_user_exist_in_db(DB_NAME, users_table, username):
+        # User already exists, return None
+        return None
+
+    # User does not exist, proceed with adding user data to the database
+    hashed_password = hash_password(password)
+    insert_new_user(DB_NAME, users_table, username, hashed_password)
