46c9a72aad3ade7d86627f0fa83c5ee9e4961762
nataliapiotrowska <nataliapiotrowska@MacBook-Air-Natalia.local>
2023-12-06 08:46:03 +0100

    setting up the file

---
+++ menus/starting_menu.py
@@ -0,0 +1,8 @@
+import sys
+from board import Board
+from menus.menu import Menu
+from models.components.button import Button
+from utils import assets_library
+
+pygame.font.init()
+font = pygame.font.Font('assets/fonts/FukuCatch.otf', 120)
