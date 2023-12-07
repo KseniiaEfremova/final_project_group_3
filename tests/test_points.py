30468d660d2d76473ae55114dece7be3bb7edee0
k.stopczynska <k.stopczynska@gmail.com>
2023-12-07 10:23:10 +0100

    added points initialization tests

--- tests/test_points.py
+++ tests/test_points.py
@@ -1,1 +1,32 @@
-# TODO: test points class
+import unittest
+from unittest.mock import MagicMock, patch
+import pygame
+from board import Board
+from models.player import Player
+from models.falling_items.falling_items_factory import FallingItemsFactory
+from models.stats.points import Points
+from utils import assets_library
+
+
+class TestPoints(unittest.TestCase):
+
+    def setUp(self):
+        pygame.init()
+        pygame.font.init()
+        self.board = pygame.display.set_mode((800, 600))
+        self.test_board = Board("Test Board", (800, 600), 60)
+        self.test_falling_items = FallingItemsFactory(self.test_board)
+        self.test_player = Player(100, 100, self.test_board, self.test_falling_items)
+        self.points = Points(self.test_player, self.test_board)
+
+    def test_level_initialization(self):
+        self.assertEqual(self.points.board_instance, self.test_board)
+        self.assertEqual(self.points.x, 500)
+        self.assertEqual(self.points.y, 75)
+        self.assertEqual(self.points.points, self.test_player.points)
+
+
+    def tearDown(self):
+        pygame.quit()
+        pygame.font.quit()
+        patch.stopall()
