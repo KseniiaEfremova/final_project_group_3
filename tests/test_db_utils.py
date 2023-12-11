20f96b6178679531c375753d06fb8a428d6f9073
k.stopczynska <k.stopczynska@gmail.com>
2023-12-11 11:46:22 +0100

    added testing exception for creating db

--- tests/test_db_utils.py
+++ tests/test_db_utils.py
@@ -22,7 +22,8 @@ class TestDatabaseConnection(unittest.TestCase):
             self.assertIsNotNone(db_connection)
 
     @patch('db.db_utils.mysql.connector.connect')
-    def test_connect_to_mysql_database(self, mock_connect):
+    def test_connect_to_mysql_database_exception(self, mock_connect):
+
         mock_connect.side_effect = mysql.connector.Error('Error connecting to the database')
 
         with self.assertRaises(mysql.connector.Error):
@@ -48,7 +49,7 @@ class TestDatabaseConnection(unittest.TestCase):
 
 
 
-    def test_create_database(self):
+    def test_create_database_success(self):
         with unittest.mock.patch(
             'db.db_utils.mysql.connector.connect',
                 return_value=self.mock_connection):
@@ -60,6 +61,14 @@ class TestDatabaseConnection(unittest.TestCase):
             expected_sql = "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(db_name)
 
             self.mock_cursor.execute.assert_called_once_with(expected_sql)
+
+    @patch('db.db_utils.mysql.connector.connect')
+    def test_create_database_exception(self, mock_connect):
+
+        mock_connect.side_effect = mysql.connector.Error('Failed creating database')
+
+        with self.assertRaises(mysql.connector.Error):
+            connect_to_mysql_database('test_db')
 
     def test_connect_to_database_or_create_if_not_exists(self):
         with unittest.mock.patch(
