dc1ef5ac998f3b04d394d9ef859c5f957e63a2a7
k.stopczynska <k.stopczynska@gmail.com>
2023-12-11 11:16:02 +0100

    changed db_utils success test cases

--- tests/test_db_utils.py
+++ tests/test_db_utils.py
@@ -11,55 +11,47 @@ class TestDatabaseConnection(unittest.TestCase):
         self.mock_connection.cursor.return_value = self.mock_cursor
 
     def test_connect_to_mysql_database(self):
-        try:
-            with unittest.mock.patch(
-                    'db.db_utils.mysql.connector.connect',
-                    return_value=self.mock_connection):
-                db_connection = connect_to_mysql_database('test_db')
-                self.assertIsNotNone(
-                    db_connection)
-                db_connection.close()
-        except Exception as e:
-            self.fail(f"Failed to connect to the database: {e}")
+        with unittest.mock.patch(
+            'db.db_utils.mysql.connector.connect',
+                return_value=self.mock_connection):
+            db_connection = connect_to_mysql_database('test_db')
+
+            self.assertIsNotNone(db_connection)
 
     def test_get_cursor_and_connection(self):
-        try:
-            with unittest.mock.patch('db.db_utils.mysql.connector.connect', return_value=self.mock_connection):
-                cursor, db_connection = get_cursor_and_connection('test_db')
-                self.assertIsNotNone(cursor)
-                self.assertIsNotNone(db_connection)
-                cursor.close()
-                db_connection.close()
-        except Exception as e:
-            self.fail(f"Failed to get cursor and connection: {e}")
+        with unittest.mock.patch('db.db_utils.mysql.connector.connect',
+                                 return_value=self.mock_connection):
+            cursor, db_connection = get_cursor_and_connection('test_db')
+
+            self.assertIsNotNone(cursor)
+            self.assertIsNotNone(db_connection)
+
+
 
     def test_create_database(self):
-        try:
-            with unittest.mock.patch(
-                'db.db_utils.mysql.connector.connect',
-                    return_value=self.mock_connection):
+        with unittest.mock.patch(
+            'db.db_utils.mysql.connector.connect',
+                return_value=self.mock_connection):
 
-                db_name = 'test_db'
+            db_name = 'test_db'
 
-                create_database(db_name)
+            create_database(db_name)
 
-                expected_sql = "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(db_name)
-                self.mock_cursor.execute.assert_called_once_with(expected_sql)
-        except Exception as e:
-            self.fail("Failed creating database: {}".format(e))
+            expected_sql = "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(db_name)
+
+            self.mock_cursor.execute.assert_called_once_with(expected_sql)
 
     def test_connect_to_database_or_create_if_not_exists(self):
-        try:
-            with unittest.mock.patch(
-                'db.db_utils.mysql.connector.connect',
-                    return_value=self.mock_connection):
+        with unittest.mock.patch(
+            'db.db_utils.mysql.connector.connect',
+                return_value=self.mock_connection):
 
-                db_name = 'test_db'
+            db_name = 'test_db'
 
-                connect_to_database_or_create_if_not_exists(db_name)
+            connect_to_database_or_create_if_not_exists(db_name)
 
-                expected_sql = "USE {}".format(db_name)
-                self.mock_cursor.execute.assert_called_once_with(expected_sql)
-        except Exception as e:
-            self.fail("Database {} does not exist.".format(db_name))
+            expected_sql = "USE {}".format(db_name)
+
+            self.mock_cursor.execute.assert_called_once_with(expected_sql)
+
 
