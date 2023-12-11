0bfc5e1babe07e33eabe5fbb62ead0573aebd3e1
k.stopczynska <k.stopczynska@gmail.com>
2023-12-11 14:51:40 +0100

    added tests for updating user statistics

--- tests/test_user.py
+++ tests/test_user.py
@@ -82,3 +82,34 @@ class TestUser(unittest.TestCase):
         result =  initial_user_statistics(self.db_name, self.statistics_table, mock_user_id, points=0, life=90, level=1)
 
         self.assertEqual(result, {'message': 'Cannot add initial user statistics, try again later'})
+
+    @patch('db.user.get_user_id')
+    def test_update_user_statistics_success(self, mock_user_id):
+        with unittest.mock.patch(
+            'db.db_utils.mysql.connector.connect',
+                return_value=self.mock_connection):
+
+            update_user_statistics(self.db_name, self.statistics_table, 900, 68, 2, mock_user_id)
+
+            expected_sql = """UPDATE {}
+        SET points = {}, life = {}, level = {}
+        WHERE user_id = {}""".format(self.statistics_table, 900, 68, 2, mock_user_id).replace(
+                "\n", "").replace(" ", "")
+
+            actual_sql = self.mock_cursor.execute.call_args[0][0].replace("\n",
+                                                                          "").replace(
+                " ", "")
+
+            self.assertEqual(expected_sql, actual_sql)
+
+    @patch('db.user.get_user_id')
+    @patch('db.history.get_cursor_and_connection')
+    def test_update_users_statistics_exception(self, mock_user_id, mock_get_cursor_and_connection):
+
+        self.mock_cursor.execute.side_effect = Exception('Cannot update user statistics right now, please try again later')
+
+        mock_get_cursor_and_connection.return_value = (
+            self.mock_cursor, self.mock_connection)
+
+        result =  update_user_statistics(self.db_name, self.statistics_table, 900, 68, 2, mock_user_id)
+        self.assertEqual(result, {'message': 'Cannot update user statistics right now, please try again later'})
