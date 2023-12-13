39ddb220d7809e3d6b4bdf812d7baea271b5c357
Kerri Tanya <kerritg1@gmail.com>
2023-12-13 15:49:45 +0000

    added docstrings to history.py

--- db/history.py
+++ db/history.py
@@ -6,6 +6,17 @@ statistics_table = "game_statistics"
 
 
 def get_history_data():
+    """
+    Retrieves historical game statistics data for the top 8 players.
+
+    Returns:
+        list: A list of tuples containing historical data for each player.
+              Each tuple includes the username, points, life, and level.
+              The list is ordered by level and points in descending order.
+    
+    Raises:
+        Exception: If there is an error retrieving data from the database.
+    """
     cursor, db_connection = get_cursor_and_connection(DB_NAME)
     try:
         query = """SELECT u.username, g.points, g.life, g.level
