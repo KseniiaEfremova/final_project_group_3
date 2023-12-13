f340f99a73c0a93ec627978add3a31ef93ab1648
Kerri Tanya <kerritg1@gmail.com>
2023-12-13 15:03:45 +0000

    added docstrings to end_game_menu.py

--- menus/end_game_menu.py
+++ menus/end_game_menu.py
@@ -11,23 +11,61 @@ font = pygame.font.Font('assets/fonts/FukuCatch.otf', 60)
 
 
 class EndGameMenu(Menu):
+	"""
+    Represents the end game menu.
+
+    Attributes:
+        play_again (bool): Flag indicating whether the player wants to play again.
+        background_pic (str): The path to the background image.
+        text (str, optional): The text to display on the menu. Defaults to None.
+	"""
+
 	def __init__(self, board_instance: Board, background_pic, text=None):
+		"""
+        Initialise the EndGameMenu.
+
+        Args:
+            board_instance (Board): The game board instance.
+            background_pic (str): The path to the background image.
+            text (str, optional): The text to display on the menu. Defaults to None.
+		"""
 		super().__init__(board_instance)
 		self.play_again = False
 		self.background_pic = background_pic
 		self.text = text
 
 	def get_play_again(self):
+		"""
+        Get the play_again status.
+
+        Returns:
+            bool: The play_again status.
+		"""
 		return self.play_again
 
 	def play_again_handler(self):
+		"""
+        Handle the play again action.
+
+        Set the play_again flag to True.
+		"""
 		self.play_again = True
 
 	def exit_game_handler(self):
+		"""
+        Handle the exit game action.
+
+        Quit the pygame application and exit the system.
+		"""
 		pygame.quit()
 		sys.exit()
 
 	def draw(self):
+		"""
+        Draw the end game menu on the board.
+
+        Display the background image, text, and buttons for play again and exit.
+		"""
 		background_image = pygame.image.load(self.background_pic)
 		image = pygame.transform.scale(background_image, (self.width, self.height))
 		if self.text is not None:
